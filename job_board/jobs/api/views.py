0c00101a2ca2093e697896a28ae3eafbba797dc5
VitttC <vittt.caria@gmail.com>
2024-12-16 17:57:40 +0100

    Added second endpoint view

--- job_board/jobs/api/views.py
+++ job_board/jobs/api/views.py
@@ -6,7 +6,9 @@ from rest_framework.views import APIView
 from jobs.models import JobOffer
 from jobs.api.serializers import JobOfferSerializer
 
-
+"""
+First endpoint: Retrieve the list of all job offers and add new ones.
+"""
 class JobOfferListCreateAPIView(APIView):
 
     def get(self, request):
@@ -20,3 +22,33 @@ class JobOfferListCreateAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+"""
+Second endpoint: Retrieve, update or delete  data of a specific job offer.
+"""
+class JobOfferDetailAPIView(APIView):
+
+    def get_object(self, pk):
+        job = get_object_or_404(JobOffer, pk=pk)
+        return job
+
+    def get(self, request, pk):
+        job = self.get_object(pk)
+        serializer = JobOfferSerializer(job)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        job = self.get_object(pk)
+        serializer = JobOfferSerializer(job, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        job = self.get_object(pk)
+        job.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
